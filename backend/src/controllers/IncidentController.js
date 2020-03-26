5a328f051ad0c6b42a0cb77804c65039e80c0293
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-03-26 20:22:50 -0300

    Add: route de delete

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,6 +1,12 @@
 const connection = require('../database/connection')
 
 module.exports = {
+    async index(request, response) {
+        const incidents = await connection('incidents').select('*')
+
+        return response.json(incidents)
+    },
+
     async create (request, response) {
         const { title, description, value} = request.body
         const ong_id = request.headers.authorization
@@ -12,5 +18,23 @@ module.exports = {
             ong_id
         })
         return response.json({ id })
+    },
+
+    async delete(request, response){
+        const { id } = request.params
+        const ong_id = request.headers.authorization // precisa pegar o ID da ong logada dentro do headers authorization
+
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first()
+
+        if (incident.ong_id != ong_id) {
+            return response.status(401).json({ erro: 'Operation not permitted.' }) // erro 401 - nao autorizado
+        }  
+
+        await connection('incidents').where('id', id).delete()
+
+        return response.status(204).send() // status resposta sem conteudo para o front end     
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,6 +8,9 @@ const routes = express.Router()
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
 
+routes.get('/incidents', IncidentController.index)
 routes.post('/incidents', IncidentController.create)
+routes.delete('/incidents/:id', IncidentController.delete)
+
 
 module.exports = routes // exportar a variavel routes para fora do arquivo
