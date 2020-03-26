d88dfe5b1a9a2747d0bbd22c1ebcf2b0c4f9e045
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-03-25 22:32:22 -0300

    Add: ongcontroller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,46 +1,11 @@
 const  express = require('express')
-const crypto = require('crypto')
 
-const connection = require('./database/connection')
+const OngController = require('./controllers/OngController')
 
 const routes = express.Router()
 
-routes.get('/ongs', async (request, response) => {
-    const ongs = await connection('ongs').select('*')
+routes.get('/ongs', OngController.index)
 
-    return response.json(ongs)
-})
+routes.post('/ongs', OngController.create)
 
-routes.post('/ongs', async (request, response) => {
-    //const params = request.query // acessar vindo da barra na rota exemplo ?name= Pedro
-    //const params = request.params
-    const { name, email, whatsapp, city, uf } = request.body
-
-    const id = crypto.randomBytes(4).toString('HEX')
-
-    console.log(name)
-    console.log(email)
-    console.log(whatsapp)
-    console.log(city)
-    console.log(uf)
-
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    }) 
-
-    return response.json({ id }) 
-})
-
-routes.post('/', (request, response) => {
-    return response.json({
-        event : "Resposta",
-        name : "Pedro" 
-    }) 
-})
-
-module.exports = routes // exortar a variavel routes para fora do arquivo
+module.exports = routes // exportar a variavel routes para fora do arquivo
