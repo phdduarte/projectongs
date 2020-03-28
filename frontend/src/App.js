ad07d287272b9138f32525bc0d4bc2d4379c2897
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-03-27 21:51:53 -0300

    Add: conceito de JSX - JavaScript mais XML

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// Quando o html esta escrito de um arquivo JS nos chamamos de JSX
+// JSX (JavaSript + XML)
+
 function App() {
     return (
         <h1>Hello World</h1>
