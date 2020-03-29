ece72dde81e6a65c89ded9a5c91bba641494fa94
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-03-29 14:00:01 -0300

    Add: Initial construction register

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
 
 import './styles.css'
 
+import logoImg from '../../assets/logo.svg'
+
 export default function Register() {
-    return <h1>Register</h1>
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Logo" />
+
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforme e ajude pessoas.</p>
+
+                    <Link className="black-link" to="/register">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </Link>
+                </section>       
+                <form>
+
+                </form> 
+            </div>    
+        </div>
+    )
 }
