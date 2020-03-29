156db15d71aae895469615fdde0bde23137b6ab9
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-03-28 23:32:09 -0300

    Fix: estilizando a pagina de login

---
+++ frontend/README.md
@@ -0,0 +1,4 @@
+instalar pacote de icons para o react
+```
+npm install react-icons
+```
--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import { FiLogIn } from 'react-icons/fi'
 
 import './styles.css'
+
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png'
 
 export default function Logon() {
     return (
         <div className="logon-container">
             <section className="form">
-                
+                <img src={logoImg} alt="Logo" />    
+                <form>
+                    <h1>Faça seu logon</h1>
+
+                    <input placeholder="Sua ID" type="text"/>
+                    <button type="submit">Entrar</button>
+
+                    <a href="/register">
+                        <FiLogIn size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </a>
+                </form> 
             </section>
+            <img src={heroesImg} alt="Heroes" />
         </div>
     )
 }
