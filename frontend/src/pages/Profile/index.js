f3104a0c6d572be7325214531de1a9b1f35eea56
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-03-29 14:48:47 -0300

    Initial: html Profile

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiPower } from 'react-icons/fi'
+
+import logoImg from '../../assets/logo.svg'
+
+export default function Profile() {
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt="Logo"/>
+                <span>Bem vinda, APAD</span>
+                <Link className="button" to="/incidents/new">
+                    Cadastrar novo caso
+                </Link>
+                <button type="button">
+                    <FiPower size={18} color="#E02021" />
+                </button>
+            </header>
+        </div>
+    )
+}
