f58235047bc06d7e89158ac717cf3108c3f6d9c5
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-03-28 23:51:23 -0300

    Add: pagina de login finalizado o estilo

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -15,7 +15,7 @@ export default function Logon() {
                     <h1>Faça seu logon</h1>
 
                     <input placeholder="Sua ID" type="text"/>
-                    <button type="submit">Entrar</button>
+                    <button className="button" type="submit">Entrar</button>
 
                     <a href="/register">
                         <FiLogIn size={16} color="#E02041" />
