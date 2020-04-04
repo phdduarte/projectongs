f0c52f018b2f10331868db1ad5e725b8c6ec7deb
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-04-03 21:29:53 -0300

    Add: Routes

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes'
 
 export default function App() {
-return (
-    <View style={styles.container}>
-    <Text style={styles.title}>Oi</Text>
-    </View>
-    );
+    return (
+        <Routes />
+    )
 }
-    
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#7159c1',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title : {
-        color: '#fff',
-        fontSize: 30,
-        fontWeight: 'bold',
-    }
-});
-  
