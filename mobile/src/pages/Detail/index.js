62878a5baa75dde8ba8387ddd44fe309ea8a170a
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-04-19 22:36:54 -0300

    Add: link do whatsapp

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Feather } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native'
+import * as MailComposer from 'expo-mail-composer'
 
 import logoImg from '../../assets/logo.png'
 
@@ -9,9 +10,22 @@ import styles from './styles'
 
 export default function Detail() {
     const navigation = useNavigation()
+    const message = 'Ola APAD'
 
     function navigateBack() {
         navigation.goBack() // funcao de dentro do navigation para voltar a tela
+    }
+
+    function sendMail() {
+        MailComposer.composeAsync({
+            subject: 'Herói do caso: Cadelinha atropelada',
+            recipients: ['pedrohlduarte@gmail.com'],
+            body: message,
+        })
+    }
+
+    function sendWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=+5511972436305&text=${message}`)
     }
 
     return (
@@ -42,11 +56,11 @@ export default function Detail() {
                 <Text style={styles.heroDescription}>Entre em contato.</Text>
 
                 <View style={styles.actions}>
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
                         <Text style={styles.actionText}>WhatsApp</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
                         <Text style={styles.actionText}>E-mail</Text>
                     </TouchableOpacity>
                 </View>  
