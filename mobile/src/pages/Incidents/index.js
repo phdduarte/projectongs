4065379c0018d9b2c2abc648945e6c33c6138e8e
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-04-23 22:00:21 -0300

    Add estado vindo do header de x total count

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -11,6 +11,8 @@ import styles from './styles'
 
 export default function Incidents() {
     const [incidents, setIncidents] = useState([])
+    const [total, setTotal] = useState(0)
+
     const navigation = useNavigation()
 
     function navigateToDetail() {
@@ -21,6 +23,7 @@ export default function Incidents() {
         const response = await api.get('incidents') 
 
         setIncidents(response.data)
+        setTotal(response.headers['x-total-count'])
     }
 
     useEffect(() => {
@@ -32,7 +35,7 @@ export default function Incidents() {
             <View style={styles.header}>
                 <Image source={logoImg} />
                 <Text style={styles.headerText}>
-                    Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+                    Total de <Text style={styles.headerTextBold}>{total} casos</Text>.
                 </Text>
             </View> 
 
@@ -53,7 +56,17 @@ export default function Incidents() {
                         <Text style={styles.incidentValue}>{incident.title}</Text>
 
                         <Text style={styles.incidentProperty}>VALOR:</Text>
-                        <Text style={styles.incidentValue}>{incident.value}</Text>
+                        <Text style={styles.incidentValue}>
+                            {
+                                Intl.NumberFormat(
+                                    'pt-BR', 
+                                        { 
+                                            style: 'currency', 
+                                            currency: 'BRL' 
+                                        }
+                                ).format(incident.value)
+                            }
+                        </Text>
 
                         <TouchableOpacity 
                             style={styles.detailsButton} 
