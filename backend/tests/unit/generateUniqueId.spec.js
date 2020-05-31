51d3c6c3721410a2bf0bf0632c8ff913807bc530
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-05-31 15:02:41 -0300

    Primeiro test unitario

--- backend/tests/unit/generateUniqueId.spec.js
+++ backend/tests/unit/generateUniqueId.spec.js
@@ -1,6 +1,10 @@
+const generateUniqueId = require('../../src/utils/generateUniqueId');
+
 describe('Generate Unique ID', () => {
     it('should generate an unique ID', () => {
-        expect(2 + 2).toBe(4);
+        const id = generateUniqueId();
+
+        expect(id).toHaveLength(8);
+
     });
-    
 });
