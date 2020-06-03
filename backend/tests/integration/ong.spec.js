9d6dc92ffe90bdcf7aac279049d5a9ba86f9536e
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-06-03 09:46:57 -0300

    Add the send data ongs

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -1,8 +1,16 @@
 const request = require('supertest');
+const app = require('../../src/app');
 
 describe('ONG', () => {
-    it('should be able to create a new ONG', () => {
-            
+    it('should be able to create a new ONG', async () => {
+        const response = await request(app)
+            .post('/ongs')
+            .send({
+                name : "APAD",
+	            email : "contato@teste.com.br",
+	            whatsapp : "23123312",
+	            city : "Belo Horizonte",
+	            uf : "MG"    
+            })   
     });
-    
 });
